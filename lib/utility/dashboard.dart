bda77b57e14754ea5f6a3f0574bc3a1383cffdd1
ramos <ramosmark833@gmail.com>
2025-03-10 13:35:59 +0800

    codemagic

--- lib/utility/dashboard.dart
+++ lib/utility/dashboard.dart
@@ -321,7 +321,7 @@ String shift = '';
                           ),
                           padding: const EdgeInsets.all(16),
                           child: Text(
-                            'HELLO $_name\nWELCOME TO SORTMATIC!',
+                            'Hello $_name,\nWelcome to SortMatic!',
                             style: const TextStyle(
                               fontSize: 12,
                               color: Colors.black,
@@ -342,7 +342,7 @@ String shift = '';
                             ),
                           ),
                           child: const Text(
-                            'MAKE THE CAMPUS CLEANER!',
+                            'Make The Campus Cleaner!',
                             style: TextStyle(
                               fontSize: 12,
                               color: Colors.black,
